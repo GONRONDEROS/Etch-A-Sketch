8d8b96637dc6e89168ef8142b4f1cde8684df94a
gonzalo <gonzalorond@gmail.com>
2022-09-08 19:53:14 -0300

    Add the mouseover effect and the reinitialization of the board for each size modification

--- app.js
+++ app.js
@@ -4,19 +4,26 @@ document.addEventListener("DOMContentLoaded", function(){
 
 function createBoard(size){
     let board = document.querySelector('.board');
-    
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns =  `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let totalOfDivs = size * size; 
     for (let i = 0; i< totalOfDivs; i++) {
         let square = document.createElement('div');
-        square.style.backgroundColor = 'red';
+        square.style.backgroundColor = "white";
+        square.addEventListener("mouseover", function(){this.style.backgroundColor = "black"});
+        
         board.insertAdjacentElement('beforeend', square);
     };
 };
 
 function getSize(input){
-    createBoard(input);
-}
+    if(input >= 2 && input <= 100){
+        createBoard(input);
+    } else {
+        console.log("Debes elegir un tamano entre 2 y 100");
+    };
+};
 
