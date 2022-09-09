a0ad49a66b9bcb5eda28fbc188f4e071d9cc4cf7
gonzalo <gonzalorond@gmail.com>
2022-09-09 10:43:58 -0300

    Give functionality to the Black, Grey, Random, Erasor and Reset buttons

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let color = 'black';
+
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
 });
@@ -27,5 +29,19 @@ function getSize(input){
 };
 
 function squareColor(){
-    this.style.backgroundColor = "black"
+    if(color === 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    };
 };
+
+function changeColor(choice){
+    color = choice;
+};
+
+function resetBoard(){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
